78ed243b30bd2a777e9755d694feb7ddd961e513
requestFocus <thezodiac@wp.pl>
2018-04-02 19:50:14 +0200

    2 kwietnia 19:50

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,5 +6,28 @@ public class Test : MonoBehaviour {
 
 	void Start () {
 		Debug.Log("test");		
+		Debug.Log("2 kwietnia, commit z domu");		
 	}
 }
+
+
+/*
+ * [linijka ===== oddziela wykonane od niewykonanych]
+ * 
+ * najpierw stworzyłem repo
+ * potem stworzyłem w nim projekt unity
+ * stworzyłem skrypt, gameobject itd., zrobiłem commit, publish repo
+ * pojawiło się na github.com
+ * dodałem drugą linijkę debug.log i ten opis
+ * ======================================================================================================
+ * W ROBOCIE po zrobieniu clone'a dodać kolejną linijkę debug.log i zacommitować + PUSH
+ * W DOMU zrobić pull (fetch + merge)
+ * następnie dodać linijkę i zacommitować + PUSH 
+ * następnie dodać linijkę i zacommitować, BEZ PUSH (obie wersje mają się od tej pory różnić)
+ * W ROBOCIE dodać linijkę i zacommitować + PUSH (nie powinno być konfliktu, w remocie będzie wersja bez dodatkowych zmian Z DOMU)
+ * W DOMU dodać linijkę i zacommitować + PUSH (powinien pojawić się konflikt, bo wersje będą się różnić)
+ * 
+ * 
+ * 
+ * na jakimś etapie pojawi się tu factory: klasa A będzie mieć dostęp do klasy B bez tworzenia instancji klasy B w sobie, póki co zabawa w zmiany
+ */ 
