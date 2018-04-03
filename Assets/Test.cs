c779e002ad6ea4d97eb0b606a52e99f9ad491f3a
thezodiac@wp.pl <01github997$>
2018-04-03 13:57:29 +0200

    3 kwietnia, 13:57, z roboty

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,6 +7,7 @@ public class Test : MonoBehaviour {
 	void Start () {
 		Debug.Log("test");		
 		Debug.Log("2 kwietnia, commit z domu");		
+		Debug.Log("3 kwietnia, commit z roboty");		
 	}
 }
 
@@ -19,8 +20,8 @@ public class Test : MonoBehaviour {
  * stworzyłem skrypt, gameobject itd., zrobiłem commit, publish repo
  * pojawiło się na github.com
  * dodałem drugą linijkę debug.log i ten opis
- * ======================================================================================================
  * W ROBOCIE po zrobieniu clone'a dodać kolejną linijkę debug.log i zacommitować + PUSH
+ * ======================================================================================================
  * W DOMU zrobić pull (fetch + merge)
  * następnie dodać linijkę i zacommitować + PUSH 
  * następnie dodać linijkę i zacommitować, BEZ PUSH (obie wersje mają się od tej pory różnić)
@@ -30,4 +31,4 @@ public class Test : MonoBehaviour {
  * 
  * 
  * na jakimś etapie pojawi się tu factory: klasa A będzie mieć dostęp do klasy B bez tworzenia instancji klasy B w sobie, póki co zabawa w zmiany
- */ 
+ */
