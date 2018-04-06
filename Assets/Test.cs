bf7b40ee4911b567765ba928d88870a41c2100b9
thezodiac@wp.pl <01github997$>
2018-04-06 08:47:06 +0200

    6 kwietnia, 8:47, z roboty

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -31,8 +31,11 @@ public class Test : MonoBehaviour {
  * następnie dodać linijkę i zacommitować, BEZ PUSH (obie wersje mają się od tej pory różnić)
  * W ROBOCIE dodać linijkę i zacommitować + PUSH (nie powinno być konfliktu, w remocie będzie wersja bez dodatkowych zmian Z DOMU)
  * W DOMU dodać linijkę i zacommitować + PUSH (powinien pojawić się konflikt, bo wersje będą się różnić) - pojawił się konflikt, patrz niżej
- * ======================================================================================================
  * w ROBOCIE dodać linijkę i zacommitować, następnie spróbować zrobić PUSH, powinien zaproponować PULL, a następnie pokazać konflikt (najprawdopodobniej tym razem HEAD będzie dotyczył brancha pracowego, a zmiany domowe remote'a)
+ *		dodalem linijke, zacommitowałem, zrobiłem PUSH, zaproponował PULL, następnie PUSH i nie było konfliktu
+ * ======================================================================================================
+ * w DOMU dodac linijkę, commit, push, pull i sprawdzić czy konflikt
+ * 
  * 
  * <<<<<<< HEAD															// <<<< HEAD - current branch, czyli ten branch, na którym aktualnie pracuję, 
 		Debug.Log("3 kwietnia, commit z domu, bez PUSH");		
@@ -41,9 +44,6 @@ public class Test : MonoBehaviour {
 		Debug.Log("4 kwietnia, commit z roboty + PUSH");				// >>>> zmiany wprowadzone na remote'ie, których nie ma na current branchu, a które wyszły dopiero po próbie PUSH/PULLA
 >>>>>>> 10bf02c8b0571a2bdba582b680a18e9605694ec7
  *
- * 
- * 
- * 
  * 
  * 
  * na jakimś etapie pojawi się tu factory: klasa A będzie mieć dostęp do klasy B bez tworzenia instancji klasy B w sobie, póki co zabawa w zmiany
